bfea182e1c0b412f682db0dbf4319a7b7e5f7aac
sudoping01 <sudoping01@gmail.com>
2024-11-11 00:26:01 +0000

    packages setup file

---
+++ AutonomousAgent/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AutonomousAgent',
+    version='0.0.1',    
+    description='caytu ai packages',
+    url='https://github.com/CAYTU',
+    author='Seydou DIALLO',
+    author_email='mail.seydou.diallo@gmail.com',
+    license='BSD 2-clause',
+    packages=find_packages(),  
+    install_requires=[
+        'google-auth-oauthlib',
+        'google-auth-httplib2',
+        'google-api-python-client',
+        'google-generativeai',
+        'AWSIoTPythonSDK==1.5.4',
+        'langchain-google-genai',
+        'langchain',
+        'openai',
+        'tiktoken',
+        'faiss-gpu',
+        'langchain_experimental',
+        'langchain[docarray]',
+        'boto3',
+        'python-dotenv' 
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+    ],
+    python_requires='>=3.8',
+)
