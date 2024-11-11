9d973253fd9e36d476766a556574d1d8dac33aed
sudoping01 <sudoping01@gmail.com>
2024-11-11 03:32:52 +0000

    update the config init

---
+++ AutonomousAgent/config/__init__.py
@@ -0,0 +1,8 @@
+from .tool_config import IOT_TOOLS, GOOGLE_TOOLS, NEWS_TOOLS, DOCUMENT_TOOL
+
+__all__ = [
+            "IOT_TOOLS",
+            "GOOGLE_TOOLS",
+            "NEWS_TOOLS",
+            "DOCUMENT_TOOL"
+        ]
